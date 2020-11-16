02203e34b13a3389d304620f01b74de11127e295
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-16 14:24:07 -0500

    created a form to store the notes according to their priority level

--- to-do/src/components/create-todo.component.js
+++ to-do/src/components/create-todo.component.js
@@ -54,8 +54,60 @@ export default class CreateTodo extends Component {
     }
     render() {
         return (
-            <div>
-                Welcome to Create Todos Component
+            <div style={{marginTop: 10}}>
+                <h3>Create new To Do</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                        className="form-control"
+                        value={this.state.todo_description}
+                        onChange={this.onChangeToDoDescription} />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input type="text"
+                        className="form-control"
+                        value={this.state.todo_responsible}
+                        onChange={this.onChangeToDoResponsible} />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                            type="radio"
+                            name="priorityOptions"
+                            id="priorityLow"
+                            value="Low"
+                            checked={this.state.todo_priority==='Low'}
+                            onChange={this.onChangeToDoPriority} />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                            type="radio"
+                            name="priorityOptions"
+                            id="priorityLow"
+                            value="Medium"
+                            checked={this.state.todo_priority==='Medium'}
+                            onChange={this.onChangeToDoPriority} />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                            type="radio"
+                            name="priorityOptions"
+                            id="priorityHigh"
+                            value="High"
+                            checked={this.state.todo_priority==='High'}
+                            onChange={this.onChangeToDoPriority} />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Todo" className="btn btn-primary" />
+                    </div>
+                </form>
+                
             </div>
         )
     }
