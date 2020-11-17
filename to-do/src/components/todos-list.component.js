97b7b4e34a85745b1ad9436cc504789805599cc5
Maurya Patel <patelmaurya0512@gmail.com>
2020-11-17 11:59:30 -0500

    connected front-end and back-end using axios, setState and more

--- to-do/src/components/todos-list.component.js
+++ to-do/src/components/todos-list.component.js
@@ -1,9 +1,63 @@
 import React , { Component } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+ props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
 export default class TodosList extends Component {
+
+    // we use the constructor to initialize the state with an empty todos array
+    constructor(props){
+        super(props)
+        this.state = {todos: []};
+    }
+
+    // to retrieve the todos data from the database the componentDidMount lifecycle is used
+    componentDidMount() {
+        // using the axios.get method to access the endpoint and once result becomes available we are assigning that to the todos array using this.setState method 
+        axios.get('http://localhost:5000/todos/')
+        .then(response => {
+            this.setState({todos: response.data});
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    todoList() {
+        return this.state.todos.map(function(currentTodo, i) {
+            return <Todo todo={currentTodo} key={i}/>;
+        })
+    }
     render() {
+        // jsx code in the return statement
         return (
-            <div>Welcome to TodosList Component</div>
+            <div>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{marginTop: 30}}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* to output a table row of each todo item */}
+                        { this.todoList()}
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
